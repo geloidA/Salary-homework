fc0a2aef6feb0fff1d0c6f8daac20abb917a492d
geloidA <alexkolpak2003@gmail.com>
2021-10-19 23:56:16 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+
+namespace SalaryHomework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var reply = new Reply();
+            SalaryInfo salaryInfo;
+            if (TryGetSalaryInfo(out salaryInfo,
+                txtBoxSalary.Text,
+                cmbBoxDistrictCoeff.Text,
+                txtBoxWorkedDays.Text,
+                txtBoxSpentDays.Text,
+                txtBoxPrize.Text))
+            {
+                reply.fullSalary.Content = salaryInfo.FullSalary.ToString();
+                reply.personalTax.Content = salaryInfo.PersonalTax.ToString();
+                reply.actualSalary.Content = salaryInfo.ActualSalary.ToString();
+                reply.Show();
+            }
+        }
+
+        private bool TryGetSalaryInfo(out SalaryInfo salaryInfo,
+            string salary,
+            string districtCoeff,
+            string workedDays,
+            string spentDays,
+            string prize)
+        {
+            try
+            {
+                salaryInfo = GetSalaryInfo(salary, districtCoeff, workedDays, spentDays, prize);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Incorrect input!");
+                salaryInfo = new SalaryInfo();
+                return false;
+            }
+            return true;
+        }
+
+        private SalaryInfo GetSalaryInfo(string salary,
+            string districtCoeff,
+            string workedDays,
+            string spentDays,
+            string prize)
+        {
+            return new SalaryInfo(double.Parse(salary),
+                double.Parse(districtCoeff) / 100,
+                double.Parse(prize),
+                int.Parse(workedDays),
+                int.Parse(spentDays));
+        }
+    }
+}
---
+++ SalaryInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SalaryHomework
+{
+    class SalaryInfo
+    {
+        private readonly double salary;
+        private readonly double districtCoeff;
+        private readonly double prize;
+        private readonly int workedDays;
+        private readonly int spentDays;
+
+        public double FullSalary => Math.Round((salary * districtCoeff * workedDays / spentDays) + prize, 2);
+
+        public double ActualSalary => Math.Round(FullSalary - PersonalTax, 2);
+
+        public double PersonalTax => Math.Round(FullSalary * 0.13, 2);
+
+        public SalaryInfo() : this(0, 0, 0, 0, 0)
+        {
+
+        }
+
+        public SalaryInfo(double salary, double districtCoeff, double prize, int workedDays, int spentDays)
+        {
+            this.salary = salary;
+            this.districtCoeff = districtCoeff;
+            this.prize = prize;
+            this.workedDays = workedDays;
+            this.spentDays = spentDays;
+        }
+    }
+}
